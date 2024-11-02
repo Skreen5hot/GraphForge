e51e516b088c31a533ab1e0079b7eb04406c7d9e
aaron <aaron.damiano@gmail.com>
2024-11-02 13:16:55 -0400

    more updates

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -4,6 +4,18 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function initializeApp() {
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('./service-worker.js').then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
+          }).catch(error => {
+            console.log('Service Worker registration failed:', error);
+          });
+        });
+      }
+      
+
     const querySectionsContainer = document.getElementById('query-sections-container');
     const addQueryButton = document.getElementById('add-query-section');
 
