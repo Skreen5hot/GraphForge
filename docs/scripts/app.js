e8855aa475ea2980780f9cb3f07bd60f7677dce2
aaron <aaron.damiano@gmail.com>
2024-11-23 05:47:52 -0500

    what is happening?

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -42,7 +42,7 @@ function initializeApp() {
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./service-worker.js?v2.51').then(registration => {
+      navigator.serviceWorker.register('./service-worker.js').then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
       }).catch(error => {
         console.log('Service Worker registration failed:', error);
