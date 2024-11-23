da569f2b0cbd37cb311518042ed9a9ba552edc93
aaron <aaron.damiano@gmail.com>
2024-11-23 04:54:16 -0500

    testing updating service worker version

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "query-browser-cache-v1.1"; //Update cache version to refresh
+const CACHE_NAME = "query-browser-cache-v2.3"; //Update cache version to refresh
 const filesToCache = [
     "./",
     "index.html",
