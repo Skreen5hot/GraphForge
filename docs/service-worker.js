2d8acf577e7128112bc3235d6ed553f52eae92f4
Aaron Damiano <aaron.damiano@gmail.com>
2025-11-13 03:49:18 -0500

    remember to increment catch

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "query-browser-cache-v2.53"; // Increment this version to force a service worker update and re-cache the app shell.
+const CACHE_NAME = "query-browser-cache-v2.54"; // Increment this version to force a service worker update and re-cache the app shell.
 const filesToCache = [
     "./",
     "index.html",
